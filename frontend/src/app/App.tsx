ac99624385af05e79a1269c9c97aa3e7d3762fd2
Nikolinc <tsigulskynikita@gmail.com>
2023-10-02 17:46:25 +0500

    moved the frontend part to a separate directory

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Tasks from 'pages/tasks/tasks';
+import Project from 'pages/projects/project';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/project" element={<Project />} />
+        <Route index element={<Project />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
