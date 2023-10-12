5619515a268a2d619b1904073054a835f880bf07
Nikolinc <tsigulskynikita@gmail.com>
2023-10-12 15:00:56 +0500

    add navbar, tasklist and project left bar

--- frontend/src/pages/tasks/tasks.tsx
+++ frontend/src/pages/tasks/tasks.tsx
@@ -1,10 +1,22 @@
+import { useSearchParams } from "react-router-dom";
 import "./tasks.css";
 import ProjectLeftBar from "components/projectLeftBar/projectLeftBar";
+import TaskList from "components/tasksList/taskList";
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import NavBar from "components/navbar/navbar";
 
 export default function Tasks() {
+
   return (
-    <div className="tasks-page">
+    <div className="tasks-page" id="tasks-page" >
       <ProjectLeftBar />
+      <div className="tasks">
+        <NavBar />
+        <DndProvider backend={HTML5Backend}>
+          <TaskList />
+        </DndProvider>
+      </div>
     </div>
   );
 }
