0c17221ba3d478ca1641ca39386ab53c21be3666
Nikolinc <tsigulskynikita@gmail.com>
2023-10-12 14:35:40 +0500

    update task type

--- frontend/src/types/tasks.ts
+++ frontend/src/types/tasks.ts
@@ -1,14 +1,30 @@
-export interface Task{
-  id:number,
-  title:string,
-  description:string,
-  createdDate:string,
-  timeAtWork:string,
-  expirationDate:string,
-  Priority:string,
-  File:string,
-  CurrentStatus:string,
-  Coments:string
+import { type } from "os";
+
+export interface Task {
+  _id: string;
+  title: string;
+  description: string;
+  createDate: string;
+  timeAtWork: string;
+  expirationDate: string;
+  priority: Priority;
+  file: string;
+  currentStatus: Status;
+  coments: string;
+  project: string;
+}
+
+export enum Priority {
+  Critical = "Critical",
+  High = "High",
+  Medium = "Medium",
+  Low = "Low",
+}
+
+export enum Status {
+  Queue = "Queue",
+  Development = "Development",
+  Done = "Done",
 }
 
 export interface TaskState {
@@ -18,16 +34,19 @@ export interface TaskState {
 }
 
 export enum TaskActionTypes {
-  TASKS_REQUEST = 'TASKS_REQUEST',
-  TASKS_SUCCESS= 'TASKS_SUCCESS',
-  TASKS_ERROR= 'TASKS_ERROR',
-  SET_TASKS_REQUEST = 'SET_TASKS_REQUEST',
-  SET_TASKS_SUCCESS = 'SET_TASKS_SUCCESS',
-  SET_TASKS_ERROR = 'SET_TASKS_ERROR',
+  TASKS_REQUEST = "TASKS_REQUEST",
+  TASKS_SUCCESS = "TASKS_SUCCESS",
+  TASKS_ERROR = "TASKS_ERROR",
+  SET_TASKS_REQUEST = "SET_TASKS_REQUEST",
+  TASKS_UPLOAD = "TASKS_UPLOAD",
 }
 
 interface TaskRequestAction {
-  type: TaskActionTypes.TASKS_REQUEST
+  type: TaskActionTypes.TASKS_REQUEST;
+}
+
+interface TaskUploadAction {
+  type: TaskActionTypes.TASKS_UPLOAD;
 }
 
 interface TaskSuccessAction {
@@ -37,28 +56,39 @@ interface TaskSuccessAction {
 
 interface TaskErrorAction {
   type: TaskActionTypes.TASKS_ERROR;
-  payload:string;
+  payload: string;
 }
 
-export interface TaskSuccessPayLoad{
-  task:Task[]
+export interface TaskSuccessPayLoad {
+  task: Task[];
 }
 
 export interface TaskFailurePayload {
-  error:string
+  error: string;
+}
+
+export interface TaskRequest {
+  type: typeof TaskActionTypes.TASKS_REQUEST;
+  payload: { id: string };
+}
+
+export interface TaskUpliadRequest {
+  type: typeof TaskActionTypes.TASKS_UPLOAD;
+  payload: { id: number; params: string; value: string | Date };
 }
 
 export type TaskSuccess = {
-  type:typeof TaskActionTypes.TASKS_SUCCESS,
-  payload:TaskSuccessPayLoad,
-}
+  type: typeof TaskActionTypes.TASKS_SUCCESS;
+  payload: TaskSuccessPayLoad;
+};
 
 export type TaskFailure = {
-  type:typeof TaskActionTypes.TASKS_ERROR,
-  payload:TaskFailurePayload,
-}
+  type: typeof TaskActionTypes.TASKS_ERROR;
+  payload: TaskFailurePayload;
+};
 
 export type TaskAction =
-      TaskRequestAction
-    | TaskSuccessAction
-    | TaskErrorAction
+  | TaskRequestAction
+  | TaskSuccessAction
+  | TaskErrorAction
+  | TaskUploadAction;
