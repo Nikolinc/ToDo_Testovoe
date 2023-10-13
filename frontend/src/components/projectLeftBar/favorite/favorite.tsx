eb929adb235a94313b16e2cde50c5131e13c6762
Nikolinc <tsigulskynikita@gmail.com>
2023-10-13 13:43:10 +0500

    add favorit project

--- frontend/src/components/projectLeftBar/favorite/favorite.tsx
+++ frontend/src/components/projectLeftBar/favorite/favorite.tsx
@@ -1,27 +1,42 @@
 import { useEffect, useRef, useState } from "react";
 import { ReactComponent as Like } from "assets/favorite.svg";
 import "./favorite.css"
+import { ProjectSelector, uploadProject } from "store/project/reducer";
+import { IProject } from "types/project";
+import { useDispatch, useSelector } from "react-redux";
 
 function Favorite() {
-
-  const [favorite, setFavorite] = useState(false);
+  const dispatch = useDispatch();
+  const project: IProject[] = useSelector(ProjectSelector.getProject);
+  const [favorit, setFavorite] = useState(project[0].favorit);
   const svgRef = useRef<SVGSVGElement>(null);
 
+  console.log(project);
+
+  const likes = async () => {
+    setFavorite(!favorit);
+    await dispatch(uploadProject({
+      id: project[0]._id,
+      params: 'favorit',
+      value: !project[0].favorit
+    }));
+
+  }
 
   useEffect(() => {
     if (!svgRef.current) return;
-    
-    if (favorite) {
+
+    if (project[0].favorit) {
       svgRef.current.style.stroke = "var(--tertiary)";
       svgRef.current.style.fill = "var(--tertiary)";
     } else {
       svgRef.current.style.removeProperty("fill");
       svgRef.current.style.removeProperty("stroke");
     }
-  }, [favorite]);
+  }, [project]);
 
   return (
-    <button className="favorite" onClick={() => setFavorite(!favorite)}>
+    <button className="favorite" onClick={() => likes()}>
       <Like ref={svgRef} />
     </button>
   );
--- frontend/src/components/projectLeftBar/projectLeftBar.tsx
+++ frontend/src/components/projectLeftBar/projectLeftBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { FindId, ProjectSelector } from "store/project/reducer";
@@ -14,7 +14,7 @@ function ProjectLeftBar() {
   const params = useParams();
   const dispatch = useDispatch();
   const project: IProject[] = useSelector(ProjectSelector.getProject);
-
+  const [image, setImage] = useState(false);
   const CreateDate = new Date(project[0].CreateDate).toUTCString();
 
   useEffect(() => {
@@ -27,7 +27,11 @@ function ProjectLeftBar() {
   return (
     <div>
       <div className="project-left-bar">
-        <img src={`http://localhost:7700/${project[0].image}`} alt="" />
+        {image ? <div className="imageError flex-center">
+          {project[0].title.replace(/\s/g, '')[0]}
+        </div> :
+          <img src={`http://localhost:7700/${project[0].image}`} alt="" onError={() => setImage(true)} />}
+
         <div className="project-info">
           <h2>
             {project[0].title} <Favorite />
